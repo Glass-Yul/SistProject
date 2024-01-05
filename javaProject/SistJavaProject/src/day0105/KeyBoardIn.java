cdeb78d166cb0cb89fff0aa5ace124dba915895f
Glass-Yul <dbfl3742@naver.com>
2024-01-05 09:40:47 +0900

    Scanner 테스트

---
+++ javaProject/SistJavaProject/src/day0105/KeyBoardIn.java
@@ -0,0 +1,23 @@
+package day0105;
+
+import java.util.Scanner;
+
+public class KeyBoardIn {
+
+	public static void main(String[] args) {
+		// 이름, 폰 번호, 주소 입력 후 출력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이름 : ");
+		String name = sc.next();
+		System.out.print("번호 : ");
+		String number = sc.next();
+		System.out.print("주소 : ");
+		String addr = sc.next();
+		
+		
+
+	}
+
+}
